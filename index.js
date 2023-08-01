3d89b1f886360c97428ccdda34b0c16c74a16ba9
KeenoFourie <keenofourie614@gmail.com>
2023-08-01 09:42:05 +0200

    FIXED EXPRESS CODE

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
 
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json())
+
 app.get("/", (req, res) => {
   res.send("Hoya Mense!");
 });
 
+app.get("/admin", (req,res)=>{
+    res.send("Hoyaaaaaa!")
+})
+
 app.post("/admin", (req, res) => {
   const user = req.body.user;
-  res.send("Hoya Mense!", user);
+  res.send(`Hoya Mense! ${user}`);
 });
 
 app.listen(port, () => {
